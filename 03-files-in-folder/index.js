89f02d9ce7ef171d11156a727672affe28a66560
sergei <kvakazuabr@yandex.ru>
2024-01-23 00:03:55 +0300

    feat: add task4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,27 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 
-function list() {
+async function list() {
   const pathDirectory = path.join(
     process.cwd(),
     '03-files-in-folder/secret-folder',
   );
-  fs.readdir(pathDirectory, { withFileTypes: true }, (err, items) => {
-    if (err) {
-      console.log(err.message);
-    }
+  try {
+    const items = await fs.readdir(pathDirectory, { withFileTypes: true });
 
-    items
-      .filter((el) => el[Object.getOwnPropertySymbols(el)[0]] === 1)
-      .forEach((el) => {
-        fs.stat(path.resolve(pathDirectory, el.name), (_err, stats) =>
-          console.log(
-            `${el.name.slice(0, el.name.lastIndexOf('.'))} - ${path
-              .extname(el.name)
-              .slice(1)} - ${stats.size / 1024}kb`,
-          ),
+    for (const item of items) {
+      if (item[Object.getOwnPropertySymbols(item)[0]] === 1) {
+        const stats = await fs.stat(path.resolve(pathDirectory, item.name));
+        console.log(
+          `${item.name.slice(0, item.name.lastIndexOf('.'))} - ${path
+            .extname(item.name)
+            .slice(1)} - ${stats.size / 1024}kb`,
         );
-      });
-  });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 list();
