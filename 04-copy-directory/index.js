2f3e14b7668745c820aba36f3a85084f723426fd
sergei <kvakazuabr@yandex.ru>
2024-01-23 02:26:29 +0300

    feat add task5

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,47 +1,44 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 
-function copyFile(oldFolder, newFolder) {
-  fs.readdir(oldFolder, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-
-    for (const file of files.filter(
-      (el) => el[Object.getOwnPropertySymbols(el)[0]] === 1,
-    )) {
-      fs.copyFile(
-        path.join(oldFolder, file.name),
-        path.join(newFolder, file.name),
-        (err) => {
-          if (err) throw err;
-          console.log('Файл успешно перемещён');
-        },
-      );
+async function copyFile(oldPath, newPath) {
+  try {
+    const files = await fs.readdir(oldPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file[Object.getOwnPropertySymbols(file)[0]] === 1) {
+        await fs.copyFile(
+          path.join(oldPath, file.name),
+          path.join(newPath, file.name),
+        );
+        console.log('Файл успешно перемещён');
+      }
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const copy = (oldFolder, newFolder) => {
+const copy = async (oldFolder, newFolder) => {
   const oldPath = path.join(process.cwd(), oldFolder);
   const newPath = path.join(process.cwd(), newFolder);
-  fs.stat(newPath, (err) => {
-    if (!err) {
-      fs.readdir(newPath, (err, files) => {
-        if (err) throw err;
 
-        for (const file of files) {
-          fs.unlink(path.join(newPath, file), (err) => {
-            if (err) throw Error('FS operation failed');
-            console.log('Файл успешно удален');
-          });
-        }
-        copyFile(oldPath, newPath);
-      });
+  try {
+    await fs.mkdir(newPath, { recursive: true });
+    const stats = await fs.stat(newPath);
+
+    if (stats) {
+      const files = await fs.readdir(newPath);
+      for (const file of files) {
+        await fs.unlink(path.join(newPath, file));
+        console.log('Файл успешно удален');
+      }
+      copyFile(oldPath, newPath);
     } else {
-      fs.mkdir(newPath, { recursive: true }, () => {
-        copyFile(oldPath, newPath);
-      });
+      copyFile(oldPath, newPath);
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 copy('04-copy-directory/files', '04-copy-directory/files-copy');
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,129 @@
+// // const { readdir, unlink, appendFile, readFile } = require('node:fs/promises');
+// // const path = require('node:path');
+
+// // async function bundleFile(folder, newFolder, file) {
+// //   const oldFolder = path.join(process.cwd(), folder);
+// //   const folderNew = path.join(process.cwd(), newFolder);
+// //   const file = path.join(newFolder, file);
+// //   await unlink(file);
+
+// //   try {
+// //     const files = await readdir(oldFolder, {
+// //       withFileTypes: true,
+// //     });
+
+// //     const filesFiltered = files.filter(
+// //       (el) =>
+// //         el[Object.getOwnPropertySymbols(el)[0]] === 1 &&
+// //         path.extname(el.name) === '.css',
+// //     );
+
+// //     for await (const file of filesFiltered) {
+// //       const contents = readFile(path.join(oldFolder, file.name), {
+// //         encoding: 'utf8',
+// //       });
+// //       appendFile(path.join(newFolder, fileile), contents);
+// //     }
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// // }
+
+// // bundleFile(
+// //   '/05-merge-styles/styles',
+// //   '/05-merge-styles/project-dist',
+// //   '/05-merge-styles/bundle.css',
+// // );
+
+// const {
+//   readdir,
+//   unlink,
+//   appendFile,
+//   readFile,
+//   stat,
+// } = require('node:fs/promises');
+// const path = require('node:path');
+
+// async function bundleFile(folder, newFolder, newFile) {
+//   // const file = await unlink(path.join(process.cwd(), newFolder, newFile));
+//   // if (!file) {
+//   //   console.log(123);
+//   // }
+//   try {
+//     const stats = await stat(path.join(process.cwd(), newFolder, newFile));
+//     console.log(stats);
+//     if (stats) {
+//       await unlink(path.join(process.cwd(), newFolder, newFile));
+//     }
+//     const files = await readdir(path.join(process.cwd(), folder), {
+//       withFileTypes: true,
+//     });
+
+//     for await (const file of files) {
+//       if (
+//         file[Object.getOwnPropertySymbols(file)[0]] === 1 &&
+//         path.extname(file.name) === '.css'
+//       ) {
+//         const contents = readFile(path.join(process.cwd(), folder, file.name), {
+//           encoding: 'utf8',
+//         });
+//         await unlink(path.join(process.cwd(), newFolder, newFile));
+//         appendFile(path.join(process.cwd(), newFolder, newFile), contents);
+//       }
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// bundleFile('/styles', '/project-dist', 'bundle.css');
+const { readdir, unlink, appendFile, readFile } = require('node:fs/promises');
+const path = require('node:path');
+
+const deleteFile = async (path) => {
+  await unlink(path);
+  console.log('file deleted');
+};
+
+const myAppendFile = async (path, data) => {
+  console.log(path);
+  try {
+    await appendFile(path, data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+async function bundleFile(folder, newFolder, newFile) {
+  const file = path.join(process.cwd(), newFolder, newFile);
+  const oldFolder = path.join(process.cwd(), folder);
+
+  deleteFile(file);
+
+  try {
+    const items = await readdir(oldFolder, {
+      withFileTypes: true,
+    });
+
+    const filesFiltered = items.filter(
+      (el) =>
+        el[Object.getOwnPropertySymbols(el)[0]] === 1 &&
+        path.extname(el.name) === '.css',
+    );
+
+    for await (const item of filesFiltered) {
+      const contents = await readFile(path.join(oldFolder, item.name), {
+        encoding: 'utf8',
+      });
+      myAppendFile(file, contents);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bundleFile(
+  '05-merge-styles/styles',
+  '05-merge-styles/project-dist',
+  'bundle.css',
+);
