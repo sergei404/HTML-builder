5abe7e1d03bbd5b25dda2357a31130d43e4ea8db
sergei <kvakazuabr@yandex.ru>
2024-01-24 00:09:29 +0300

    fix: fix errors

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,21 @@
 const readline = require('node:readline');
 const fs = require('node:fs');
-const path = require('path');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 console.log('Welcome!');
-
 function question(filePath) {
-  const mypath = path.join(process.cwd(), filePath);
   rl.question('Enter text: ', (text) => {
     if (text === 'exit') {
       console.log('Exit goodbye!');
       process.exit();
     }
-    fs.appendFile(mypath, text, 'utf-8', (err) => {
+    fs.appendFile(filePath, text + '\n', 'utf-8', (err) => {
       if (err) {
         console.log(err.message);
       }
     });
-    question(mypath);
+    question(filePath);
   });
 }
 
